ae3fb56e4331bfb9887f817af1faca2c8425997f
Kenny Cartman <lyuholu@gmail.com>
2024-06-05 14:17:33 +0800

    fix: set up TS properly

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,9 @@
-console.log("[Course Number 2 Title]");
-var body = document.querySelector('body');
-body.addEventListener('dblclick', function (e) {
+"use strict";
+console.log(`[Course Number 2 Title]`);
+const body = document.querySelector('body');
+body.addEventListener('dblclick', (e) => {
     var _a, _b;
-    var selectedText = (_b = (_a = window.getSelection()) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : '';
-    var courseNumber = Number(selectedText);
+    const selectedText = (_b = (_a = window.getSelection()) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : '';
+    const courseNumber = Number(selectedText);
     console.log(courseNumber);
 });
